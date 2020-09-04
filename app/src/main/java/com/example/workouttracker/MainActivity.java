10c26e6e95d6ed3a2e45d7e6d7734aab5f7f996e
Mishel Kvitelashvili <brickpedit@gmail.com>
2020-09-04 19:24:22 +0300

    Creating Home Screen Buttons & Creating main 4 activity's
    -Added 4 Home page buttons and set their onclick listeners to lead to each's activity
    
    -Added the Begin, Edit, Track and settings activities XML files & java Classes

--- app/src/main/java/com/example/workouttracker/MainActivity.java
+++ app/src/main/java/com/example/workouttracker/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.workouttracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Getting the homepage's buttons
+        Button btnBeginWorkout = findViewById(R.id.btn_begin);
+        Button btnEditWorkout = findViewById(R.id.btn_edit);
+        Button btnTrackWorkout = findViewById(R.id.btn_track);
+        Button btnSettings = findViewById(R.id.btn_settings);
+
+        //Setting the Home Page's Buttons listeners
+        btnBeginWorkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BeginWorkoutActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnEditWorkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EditWorkoutActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnTrackWorkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TrackWorkoutActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/workouttracker/TrackWorkoutActivity.java
@@ -0,0 +1,16 @@
+package com.example.workouttracker;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TrackWorkoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trackworkouts);
+
+    }
+}
