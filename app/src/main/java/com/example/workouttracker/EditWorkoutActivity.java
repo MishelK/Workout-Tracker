cdaaf8023956bdee5b9c2a074a671632ffc9d3e5
Mishel Kvitelashvili <brickpedit@gmail.com>
2020-09-08 01:39:41 +0300

    Added "Add Workout" Dialog
    - "Add Workout" dialog added, "Close" Button now dissmisses the dialog.

--- app/src/main/java/com/example/workouttracker/EditWorkoutActivity.java
+++ app/src/main/java/com/example/workouttracker/EditWorkoutActivity.java
@@ -1,12 +1,17 @@
 package com.example.workouttracker;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Objects;
 
 public class EditWorkoutActivity extends AppCompatActivity {
 
@@ -22,8 +27,39 @@ public class EditWorkoutActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(EditWorkoutActivity.this, MainActivity.class);
                 startActivity(intent);
+
+            }
+        });
+
+        //Setting Add Workout Button
+        Button btnAddWorkout = findViewById(R.id.btn_add_new_workout);
+        btnAddWorkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSettingsDialog();
             }
         });
 
     }
+
+
+    private void openSettingsDialog() {
+
+        final Dialog dialog = new Dialog(EditWorkoutActivity.this);
+        dialog.setContentView(R.layout.dialog_addworkout);
+        Objects.requireNonNull(dialog.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.setCancelable(true);
+        dialog.show();
+
+        Button closeBtn = dialog.findViewById(R.id.btn_close);
+        closeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+
+    }
+
 }
