3422db73b1b90c476c874d90c86629e7f22df798
Mishel Kvitelashvili <brickpedit@gmail.com>
2020-09-09 18:07:54 +0300

    Data Modification Functions Added
    - Workouts added to the database can now be updated and deleted by using the "ID" unique key

--- app/src/main/java/com/example/workouttracker/DatabaseHelper.java
+++ app/src/main/java/com/example/workouttracker/DatabaseHelper.java
@@ -2,6 +2,7 @@ package com.example.workouttracker;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -57,6 +58,32 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
         return result != -1;
     }
+
+    public boolean updateWorkout(String id, String name, String description) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1, id);
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, description);
+        long result = db.update(WORKOUT_TABLE_NAME, contentValues, COL_1 + " = ?", new String[] { id });
+        return result != 0;
+    }
+
+    public boolean deleteWorkout(String id) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        long result = db.delete(WORKOUT_TABLE_NAME, COL_1 + " = ?", new String[] { id });
+        return result != 0;
+    }
+
+    public Cursor getAllWorkoutData() {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor result = db.rawQuery("SELECT * FROM " + WORKOUT_TABLE_NAME, null); //Gets all data from table
+        return result;
+    }
+
 
 
 }
--- app/src/main/java/com/example/workouttracker/SettingsActivity.java
+++ app/src/main/java/com/example/workouttracker/SettingsActivity.java
@@ -1,11 +1,15 @@
 package com.example.workouttracker;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -25,5 +29,82 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
+        final EditText etId = findViewById(R.id.et_update_id);
+        final EditText etName = findViewById(R.id.et_update_name);
+        final EditText etDesc = findViewById(R.id.et_update_desc);
+
+        //View all data section
+        Button btnViewAll = findViewById(R.id.btn_view_data);
+        btnViewAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Cursor result = DatabaseHelper.getInstance(SettingsActivity.this).getAllWorkoutData();
+
+                if(result.getCount() == 0) {
+                    showMessage("Error", "Nothing found");
+                    return;
+                }
+
+                StringBuffer buffer = new StringBuffer();
+                while (result.moveToNext()) { // will be false when we passed the last result
+
+                    buffer.append("Id : "+ result.getString(0)+ "\n");
+                    buffer.append("Name : "+ result.getString(1)+ "\n");
+                    buffer.append("Description : "+ result.getString(2)+ "\n\n");
+
+                }
+
+                showMessage("Data", buffer.toString());
+            }
+        });
+        //End of view all data section
+
+        //Update data section
+
+        Button btnUpdateData = findViewById(R.id.btn_update_data);
+        btnUpdateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                boolean isUpdated = DatabaseHelper.getInstance(SettingsActivity.this).updateWorkout(etId.getText().toString(), etName.getText().toString(), etDesc.getText().toString());
+
+                if(isUpdated)
+                    Toast.makeText(SettingsActivity.this, "Update Successful", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(SettingsActivity.this, "Update Failed", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        //End of update data section
+
+        //Delete data section
+
+        Button btnDeleteData = findViewById(R.id.btn_delete_data);
+        btnDeleteData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isDeleted = DatabaseHelper.getInstance(SettingsActivity.this).deleteWorkout(etId.getText().toString());
+
+                if(isDeleted)
+                    Toast.makeText(SettingsActivity.this, "Delete Successful", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(SettingsActivity.this, "Delete Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //End of delete data section
+
     }
+
+    public void showMessage(String title, String message) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
+    }
+
+
 }
