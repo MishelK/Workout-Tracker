d9f38bec5caabf3b61360a9f6eae42722a050c31
Mishel Kvitelashvili <brickpedit@gmail.com>
2020-09-10 21:03:53 +0300

    Workout Preview List Added
    - Added a programatically create  ScrollView that will include all workouts in the database in a preview form
    - Set the preview list to update after adding a new workout

--- app/src/main/java/com/example/workouttracker/EditWorkoutActivity.java
+++ app/src/main/java/com/example/workouttracker/EditWorkoutActivity.java
@@ -1,17 +1,26 @@
 package com.example.workouttracker;
 
 import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
 
 import java.util.Objects;
 
@@ -21,6 +30,8 @@ public class EditWorkoutActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editworkout);
+
+        LoadWorkoutPreview(); // Loading workout preview list
 
         //Setting Return Button listener
         Button btnReturn = findViewById(R.id.btn_return_edit);
@@ -38,14 +49,14 @@ public class EditWorkoutActivity extends AppCompatActivity {
         btnAddWorkout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openSettingsDialog();
+                openAddWorkoutDialog();
             }
         });
 
     }
 
 
-    private void openSettingsDialog() {
+    private void openAddWorkoutDialog() {
 
         final Dialog dialog = new Dialog(EditWorkoutActivity.this);
         dialog.setContentView(R.layout.dialog_addworkout);
@@ -74,15 +85,19 @@ public class EditWorkoutActivity extends AppCompatActivity {
                 name = workoutNameEt.getText().toString();
                 description = workoutDescEt.getText().toString();
 
-                if(name.isEmpty()) { //Checking if user has entered a name
+                if(name.isEmpty()) { // Checking if user has entered a name
+
                     Toast.makeText(EditWorkoutActivity.this, R.string.toast_data_empty_field, Toast.LENGTH_SHORT).show();
                     return;
                 }
 
-                boolean isInserted = DatabaseHelper.getInstance(EditWorkoutActivity.this).addWorkout(name,description); //Sending the new workout data to insert data method
+                boolean isInserted = DatabaseHelper.getInstance(EditWorkoutActivity.this).addWorkout(name,description); // Sending the new workout data to insert data method
 
-                if(isInserted)
+                if(isInserted) {
                     Toast.makeText(EditWorkoutActivity.this, R.string.toast_data_add_success, Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                    LoadWorkoutPreview(); // After successfully adding a new workout we want to reload the workout preview list
+                }
                 else
                     Toast.makeText(EditWorkoutActivity.this, R.string.toast_data_add_failed, Toast.LENGTH_SHORT).show();
 
@@ -91,6 +106,37 @@ public class EditWorkoutActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    public void LoadWorkoutPreview() {
+
+        Cursor result = DatabaseHelper.getInstance(EditWorkoutActivity.this).getAllWorkoutData();
+
+        if(result.getCount() > 0) { // Checking if there are any workouts in the database
+
+            LinearLayout workoutPreviewsLayout = findViewById(R.id.ll_workout_preview); // Linear Layout that will contain all sub-RelativeLayouts
+            workoutPreviewsLayout.removeAllViews();
+            TextView nameTv, descTv;
+
+            LayoutInflater layoutInflater;
+            layoutInflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            layoutInflater = LayoutInflater.from(EditWorkoutActivity.this);
+
+            RelativeLayout workoutPreview;
+
+            while (result.moveToNext()) { // will be false when we passed the last result
+
+                workoutPreview = (RelativeLayout) layoutInflater.inflate(R.layout.layout_workout_preview, null);
+
+                nameTv = workoutPreview.findViewById(R.id.tv_workout_preview_name);
+                nameTv.setText(result.getString(1));
+                descTv = workoutPreview.findViewById(R.id.tv_workout_preview_desc);
+                descTv.setText(result.getString(2));
+
+                workoutPreviewsLayout.addView(workoutPreview);
+
+            }
+        }
 
     }
 
